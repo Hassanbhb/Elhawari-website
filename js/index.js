bc862ccc6ed9c5a1d339f1d9e77ac16a57bd0553
Hassanbhb <bussinesbhb@gmail.com>
2020-03-08 15:49:55 +0100

    removed highway js

--- js/index.js
+++ js/index.js
@@ -1,30 +1,78 @@
-import Highway from "@dogstudio/highway";
-import Fade from "./transition";
-import HomeRenderer from "./renderers/home-renderer";
-import MenuRenderer from "./renderers/menu-renderer";
+import gsap from "gsap";
 
-const links = document.querySelectorAll("nav a");
-
-const H = new Highway.Core({
-  renderers: {
-    home: HomeRenderer,
-    location: HomeRenderer,
-    menu: MenuRenderer
-  },
-  transitions: {
-    default: Fade
-  }
+const nav_links = document.querySelectorAll(".nav-links");
+nav_links.forEach(link => {
+  link.addEventListener("click", e => {
+    const current_active = document.querySelector(".is-active");
+    if (Array.from(link.classList).includes("is-active")) {
+      link.classList.remove("is-active");
+    }
+    current_active.classList.remove("is-active");
+    link.classList.add("is-active");
+    transition(link);
+  });
 });
 
-H.on("NAVIGATE_IN", ({ to, location }) => {
-  //chack active link
-  links.forEach(link => {
-    //clean class
-    link.classList.remove("is-active");
+function transition(active_section) {
+  // animations that run on transition
+  gsap.fromTo(
+    ".default-animation",
+    { opacity: 0, y: 15 },
+    { opacity: 1, y: 0, duration: 0.8 }
+  );
+  // if page is location page hide the other two same logic for the other two pages
+  if (Array.from(active_section.classList).includes("location_link")) {
+    gsap.fromTo(".location", { opacity: 0 }, { opacity: 1, display: "block" });
+    gsap.fromTo(".home", { opacity: 1 }, { opacity: 0, display: "none" });
+    gsap.fromTo(".menu", { opacity: 1 }, { opacity: 0, display: "none" });
+  } else if (Array.from(active_section.classList).includes("home_link")) {
+    gsap.fromTo(".home", { opacity: 0 }, { opacity: 1, display: "block" });
+    gsap.fromTo(".location", { opacity: 1 }, { opacity: 0, display: "none" });
+    gsap.fromTo(".menu", { opacity: 1 }, { opacity: 0, display: "none" });
+  } else if (Array.from(active_section.classList).includes("menu_link")) {
+    gsap.fromTo(".menu", { opacity: 0 }, { opacity: 1, display: "block" });
+    gsap.fromTo(".location", { opacity: 1 }, { opacity: 0, display: "none" });
+    gsap.fromTo(".home", { opacity: 1 }, { opacity: 0, display: "none" });
+  }
+}
 
-    //active link
-    if (link.href === location.href) {
-      link.classList.add("is-active");
-    }
+// menu btns
+const menu_menu_btns = document.querySelectorAll(".menu_menu > button");
+const menu_sections = document.querySelectorAll(".menu-presentation > section");
+menu_menu_btns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    const clicked_btn = e.target;
+    const active_btn = document.querySelector(".active_btn");
+    const sections_arr = Array.from(menu_sections); //transform nodeList to array
+    // get the section that matches the clicked button
+    const matched_section = sections_arr.find(
+      element => element.classList[0] === clicked_btn.innerHTML.toLowerCase()
+    );
+
+    // animations
+    gsap.fromTo(
+      `.${active_btn.innerHTML.toLowerCase()}`,
+      { opacity: 1, display: "block" },
+      {
+        opacity: 0,
+        display: "none",
+        duration: 0.5,
+        onStart: () => {
+          active_btn.classList.remove("active_btn");
+          clicked_btn.classList.add("active_btn");
+        }
+      }
+    );
+
+    gsap.fromTo(
+      `.${matched_section.className}`,
+      { opacity: 0, display: "none" },
+      {
+        opacity: 1,
+        display: "block",
+        duration: 0.5,
+        delay: 0.5
+      }
+    );
   });
 });
